f861c08683584ed191473d75eba1c635b1a7dded
Alessandro Paris <alessandro1.paris@epitech.eu>
2026-04-11 09:32:18 +0200

    add: onload function, save data, reset score and add passive function

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,37 @@
-let score = 0;
+window.onload = function () {
 
-const marvin = document.getElementById("marvin");
-const scoreDisplay = document.getElementById("score");
+    let score = parseInt(localStorage.getItem("score")) || 0;
+    let passiveScore = parseInt(localStorage.getItem("passive")) || 0;
 
-marvin.addEventListener("click", () => {
-    score++;
+    const marvin = document.getElementById("marvin");
+    const scoreDisplay = document.getElementById("score");
+
     scoreDisplay.textContent = score;
-});
+
+    marvin.addEventListener("click", () => {
+        score++;
+        passiveScore += 1;
+        scoreDisplay.textContent = score;
+        localStorage.setItem("score", score);
+    });
+
+    setInterval(() => {
+        score += passiveScore;
+        scoreDisplay.textContent = score;
+        localStorage.setItem("score", score);
+    }, 1000);
+};
+
+function resetScore() {
+    localStorage.setItem("passive", 0);
+    localStorage.setItem("score", 0);
+    location.reload();
+}
+
+function addPassive() {
+    let passiveScore = parseInt(localStorage.getItem("passive")) || 0;
+    passiveScore += 1;
+    localStorage.setItem("passive", passiveScore);
+    location.reload();
+}
+
